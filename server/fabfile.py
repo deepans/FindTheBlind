56c995f98882b62a2e73d0ef1304ab46a1a244fb
deepans <deepans@thoughtworks.com>
2011-05-18 08:26:56 +0530

    included user creation in setup db

--- server/fabfile.py
+++ server/fabfile.py
@@ -1,18 +1,19 @@
 import settings
 from fabric.api import local, hide
 
-def reset_db():
-    with hide('stdout', 'stderr'):
+def setup_db():
+    with hide('stdout'):
         local("export PGPASSWORD='{0}'".format(settings.DATABASES['default']['PASSWORD']))
         local('sudo -u postgres psql postgres -c "drop database if exists {0};"'.format(settings.DATABASES['default']['NAME']))
+        local('sudo -u postgres psql postgres -c "drop user if exists {0};"'.format(settings.DATABASES['default']['USER']))
+        local('sudo -u postgres psql postgres -c "create user {0} with password \'{1}\' CREATEDB;"'.format(settings.DATABASES['default']['USER'], settings.DATABASES['default']['PASSWORD']))
         local('sudo -u postgres psql postgres -c "create database {0};"'.format(settings.DATABASES['default']['NAME']))
         local('sudo -u postgres psql postgres -c "grant all privileges on database {0} to {1};"'.format(settings.DATABASES['default']['NAME'], settings.DATABASES['default']['USER']))
-
 def sync_db():
     local('python manage.py syncdb --noinput')
 
 def db():
-    reset_db()
+    setup_db()
     sync_db()
 
 def test():
