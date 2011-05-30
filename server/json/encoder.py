39d4509ab843c26de0e138da31fecec43275f565
Deepan Subramani <emaildeepan@yahoo.co.in>
2011-05-30 22:15:31 +0530

    Customizing Json encoding

---
+++ server/json/encoder.py
@@ -0,0 +1,52 @@
+"""New base serializer class to handle full serialization of model objects."""
+try:
+    from cStringIO import StringIO
+except ImportError:
+    from StringIO import StringIO
+
+from wadofstuff.django.serializers import json
+    
+class Serializer(json.Serializer):
+    def serialize(self, queryset, **options):
+        """Serialize a queryset with the following options allowed:
+            fields - list of fields to be serialized. If not provided then all
+            fields are serialized.
+            excludes - list of fields to be excluded. Overrides ``fields``.
+            relations - list of related fields to be fully serialized.
+            extras - list of attributes and methods to include.
+            Methods cannot take arguments.
+        """
+        self.options = options
+        self.stream = options.pop("stream", StringIO())
+        self.fields = options.pop("fields", [])
+        self.excludes = options.pop("excludes", [])
+        self.relations = options.pop("relations", [])
+        self.extras = options.pop("extras", [])
+        self.use_natural_keys = options.pop("use_natural_keys", False)
+
+        self.start_serialization()
+        for obj in queryset:
+            self.start_object(obj)
+            for field in obj._meta.local_fields:
+                attname = field.attname
+                if field.serialize:
+                    if field.rel is None:
+                        if attname not in self.excludes:
+                            if not self.fields or attname in self.fields:
+                                self.handle_field(obj, field)
+                    else:
+                        if attname[:-3] not in self.excludes:
+                            if not self.fields or attname[:-3] in self.fields:
+                                self.handle_fk_field(obj, field)
+            for field in obj._meta.many_to_many:
+                if field.serialize:
+                    if field.attname not in self.excludes:
+                        if not self.fields or field.attname in self.fields:
+                            self.handle_m2m_field(obj, field)
+            for extra in self.extras:
+                self.handle_extra_field(obj, extra)
+            self.end_object(obj)
+        self.end_serialization()
+        return self.getvalue()
+
+        
