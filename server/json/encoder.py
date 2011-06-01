42d24a3bbafd8052cdf0d0733cf2e8ff3880eda8
deepans <deepans@thoughtworks.com>
2011-06-01 09:49:36 +0530

    Json serialization of Reverse one to one relation seems to work

--- server/json/encoder.py
+++ server/json/encoder.py
@@ -4,10 +4,11 @@ try:
 except ImportError:
     from StringIO import StringIO
 
-from wadofstuff.django.serializers import json
+from wadofstuff.django.serializers import python
 from django.utils.encoding import smart_unicode
-    
-class Serializer(json.Serializer):
+from django.db.models.fields.related import OneToOneField
+
+class Serializer(python.Serializer):
     def serialize(self, queryset, **options):
         """Serialize a queryset with the following options allowed:
             fields - list of fields to be serialized. If not provided then all
@@ -44,40 +45,63 @@ class Serializer(json.Serializer):
                     if field.attname not in self.excludes:
                         if not self.fields or field.attname in self.fields:
                             self.handle_m2m_field(obj, field)
+            for relation in obj._meta.get_all_related_objects():
+                if isinstance(relation.field, OneToOneField):
+                    if relation.field.serialize:
+                        if relation.var_name not in self.excludes:
+                            if not self.fields or relation.var_name in self.fields:
+                                self.handle_reverse_relation(obj, relation)
             for extra in self.extras:
                 self.handle_extra_field(obj, extra)
             self.end_object(obj)
         self.end_serialization()
         return self.getvalue()
-
-    def handle_fk_field(self, obj, field):
+    
+    def handle_reverse_relation(self, obj, relation):
         """
-        Called to handle a ForeignKey field.
+        Called to handle a reverse relations.
         Recursively serializes relations specified in the 'relations' option.
         """
-        fname = field.name
+        fname = relation.var_name
         related = getattr(obj, fname)
         if related is not None:
             if fname in self.relations:
-                # perform full serialization of FK
+                # perform full serialization of relation
                 serializer = Serializer()
                 options = {}
                 if isinstance(self.relations, dict):
                     if isinstance(self.relations[fname], dict):
                         options = self.relations[fname]
-                print type(serializer.serialize([related], **options))
                 self._fields[fname] = serializer.serialize([related], **options)[0]
             else:
                 # emulate the original behaviour and serialize the pk value
                 if self.use_natural_keys and hasattr(related, 'natural_key'):
                     related = related.natural_key()
                 else:
-                    if field.rel.field_name == related._meta.pk.name:
-                        # Related to remote object via primary key
-                        related = related._get_pk_val()
-                    else:
-                        # Related to remote object via other field
-                        related = smart_unicode(getattr(related, field.rel.field_name), strings_only=True)
+                    # Related to remote object via primary key
+                    related = related._get_pk_val()
                 self._fields[fname] = related
         else:
             self._fields[fname] = smart_unicode(related, strings_only=True)
+
+from django.utils import simplejson
+from django.core.serializers.json import DjangoJSONEncoder
+
+class JsonEncoder(Serializer):
+    """
+    Convert a queryset to JSON.
+    """
+    def end_serialization(self):
+        """Output a JSON encoded queryset."""
+        simplejson.dump(self.objects, self.stream, cls=DjangoJSONEncoder,
+                        **self.options)
+
+    def getvalue(self):
+        """
+        Return the fully serialized queryset (or None if the output stream
+        is not seekable).
+        """
+        if callable(getattr(self.stream, 'getvalue', None)):
+            return self.stream.getvalue()
+            
+
