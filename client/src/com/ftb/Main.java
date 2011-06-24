9e083d99545fa2f713e8b7d357126e8891c7a8e9
SATYA <satya@ubuntu.ubuntu-domain>
2011-06-24 12:44:38 +0530

    testing

--- client/src/com/ftb/Main.java
+++ client/src/com/ftb/Main.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 
 public class Main extends Activity
 {
-    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
