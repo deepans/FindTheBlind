908ba6941a66485a28f980e038839589fc8192ac
rainbow <rainbow@rainbow-PC.(none)>
2011-08-24 21:41:24 +0530

    Midded out library in requirement.txt, fixed import in models.py

--- server/ftb/models.py
+++ server/ftb/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from choices import VISUAL_LOSS_AGE_CHOICES, GENDER_CHOICES
 from locking.models import LockableModel
 from ftb.mixins import ConcurrentlyModifiable
-from json.mixins import JsonEncodable
+from server.json.mixins import JsonEncodable
 from django.core.exceptions import ValidationError
 
 class Patient(LockableModel, ConcurrentlyModifiable, JsonEncodable):
